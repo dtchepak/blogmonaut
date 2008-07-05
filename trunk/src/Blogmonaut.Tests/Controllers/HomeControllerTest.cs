abcb164e440b9700be582d3f2f6654b621823549
tchepak <tchepak@7924ad5f-1451-0410-93b7-7bd6baf13897>
2008-07-05 11:50:06 +0000

    Initial commit. New ASP.NET MVC project and MbUnit test project created from VS template. Added solution and tools (for binary dependencies).

---
+++ trunk/src/Blogmonaut.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Gallio.Framework;
+using MbUnit.Framework;
+
+namespace Blogmonaut.Tests.Controllers {
+    /// <summary>
+    /// Summary description for HomeControllerTest
+    /// </summary>
+    [TestFixture]
+    public class HomeControllerTest {
+        [Test]
+        public void About() {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [Test]
+        public void Index() {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
---
+++ trunk/src/Blogmonaut.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Blogmonaut.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blogmonaut.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("944e0b9f-74ba-4bf5-bf7d-d9a303d3493b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
